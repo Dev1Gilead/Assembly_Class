49998a4071108fe08c6f0d853053320cc63184ed
GSTER <gster0909@gmail.com>
2024-01-23 16:31:12 -0800

    Project complete

--- Conversion.cpp
+++ Conversion.cpp
@@ -3,8 +3,7 @@
 #include <vector>
 #include <unordered_set>
 #include <unordered_map>
-
-//#include <cmath>
+#include <cmath>
 
 std::string getUserInput() {
     std::string userInput = "";
@@ -18,7 +17,7 @@ void displayGreeting() {
 }
 bool notHex(std::string userInput) {
     //checks if every ch is one of the possible hex digits
-    std::unordered_set<char> allHexDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    std::unordered_set<char> allHexDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F','a','b','c','d','e','f'};
     for (char ch : userInput) {
         if (allHexDigits.count(ch) > 0) {
             continue;
@@ -52,7 +51,8 @@ std::string hexToBi(std::string hexNum) {
     {'0', "0000"}, {'1', "0001"},{'2', "0010"},{'3', "0011"},{'4', "0100"},
     {'5', "0101"},{'6', "0110"},{'7', "0111"},{'8', "1000"},{'9', "1001"},
     {'A', "1010"},{'B', "1011"},{'C', "1100"},{'D', "1101"},
-    {'E', "1110"},{'F', "1111"}
+    {'E', "1110"},{'F', "1111"}, {'a', "1010"},{'b', "1011"},{'c', "1100"},{'d', "1101"},
+    {'e', "1110"},{'f', "1111"}
     };
 
     std::string binaryResult = "";
@@ -71,7 +71,8 @@ std::string BiToHex(std::string biNum) {
     {"0000", '0'}, {"0001", '1'}, {"0010", '2'}, {"0011", '3'}, {"0100", '4'},
     {"0101", '5'}, {"0110", '6'}, {"0111", '7'}, {"1000", '8'}, {"1001", '9'},
     {"1010", 'A'}, {"1011", 'B'}, {"1100", 'C'}, {"1101", 'D'},
-    {"1110", 'E'}, {"1111", 'F'}
+    {"1110", 'E'}, {"1111", 'F'},{"1010", 'a'}, {"1011", 'b'}, {"1100", 'c'}, {"1101", 'd'},
+    {"1110", 'e'}, {"1111", 'f'}
     };
 
     int index = biNum.length() / 4;
@@ -87,6 +88,65 @@ std::string BiToHex(std::string biNum) {
     return(hexNum);
 }
 
+std::string hexToDec(std::string hexNum) {
+    std::unordered_map<char, std::string> allHexToDecDigis = {
+    {'0', "0"}, {'1', "1"},{'2', "2"},{'3', "3"},{'4', "4"},
+    {'5', "5"},{'6', "6"},{'7', "7"},{'8', "8"},{'9', "9"},
+    {'A', "10"},{'B', "11"},{'C', "12"},{'D', "13"},
+    {'E', "14"},{'F', "15"}, {'a', "10"},{'b', "11"},{'c', "12"},{'d', "13"},
+    {'e', "14"},{'f', "15"}
+    };
+    int j = 0;
+    std::string decNumStr = "";
+    int decNumInt = 0;
+    //the algorithm for converting Hex to Dec, goes from string, to int, and back to string.
+    for (int i = hexNum.size() - 1; i >= 0; i--)
+    {
+        int tempNum = stoi(allHexToDecDigis[hexNum[i]]);
+        decNumInt += tempNum * static_cast<int>(std::pow(16, j));
+        j++;
+    }
+    return(decNumStr.append(std::to_string(decNumInt)));
+}
+
+bool isNumber(std::string DecNum) {
+    std::unordered_set<char> allBiDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+
+    //checks if every ch is one of the possible digits
+    for (char ch : DecNum) {
+        if (allBiDigits.count(ch) > 0) {
+            continue;
+        }
+        else {
+            std::cout << "Not a Decimal Number input, or is a negitave Decimal Number.\n";
+            return false;
+        }
+    }
+    return true;
+}
+
+
+std::string DecToHex(std::string decNumStr) {
+    std::unordered_map<std::string, char> allDecToHex = {
+        {"0", '0'}, {"1", '1'}, {"2", '2'}, {"3", '3'}, {"4", '4'},
+        {"5", '5'}, {"6", '6'}, {"7", '7'}, {"8", '8'}, {"9", '9'},
+        {"10", 'A'}, {"11", 'B'}, {"12", 'C'}, {"13", 'D'},
+        {"14", 'E'}, {"15", 'F'}
+    };
+
+    std::string hexNumStr = "";
+    int j = 0;
+    int decNumInt = stoi(decNumStr);
+    //algorithm to convert dec to hex.
+    while (decNumInt > 0) {
+        int tempMod = decNumInt % 16;
+        decNumInt /= 16;
+
+        hexNumStr = allDecToHex[std::to_string(tempMod)] + hexNumStr;
+    }
+    return(hexNumStr);
+}
 int main() {
     std::string userInput;
     bool runningMenu = true;
@@ -122,17 +182,33 @@ int main() {
         }
 
         else if (userInput == "HD") {
+            std::cout << "Enter a Hexidecimal number (Do not include the 0x part):";
 
+            std::string hexNum = getUserInput();
+            if (notHex(hexNum)) {
+                continue;
+            }
+            else {
+                std::cout << "The binary is: " << hexToDec(hexNum) << std::endl;
+            }
 
         }
         else if (userInput == "DH") {
+            std::cout << "Enter a Decimal number:";
+            std::string DecNum = getUserInput();
+            if (isNumber(DecNum))
+            {
+                std::string hexNum = DecToHex(DecNum);
 
+                std::cout << "The hexadecimal number is: 0x" << hexNum << std::endl;
+
+            }
         }
         else if (userInput == "Quit" || userInput == "quit") {
             break;
         }
         else {
-            std::cout << "Invalid input";
+            std::cout << "Invalid input \n";
         }
     }
 }
